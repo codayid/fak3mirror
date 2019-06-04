8736105c85a1a8b50e05e3f7040f8412466d308b
Muhammad Hidayat <49159167+codayid@users.noreply.github.com>
2019-06-05 01:21:31 +0700

    Create fak3mirror.sh

---
+++ fak3mirror.sh
@@ -0,0 +1,34 @@
+#! usr/bin/bash
+#Hapus Copyright? Lu Bukan Coder Bro!
+
+clear
+
+figlet F4K#mirror
+echo "///////////////////////////////////////
+// AUTHOR : CodayID
+///////////////////////////////////////
+// FAKE DEFACE STOK:v
+//////////////////////////////////////
+// TEH Squad Cyber - Security Darknet
+/////////////////////////////////////"
+echo "// WEB TANPA http/www/https"
+read -p "// Masukan Website         : " web;
+read -p "// Masukan Subdomain Bebas : " sub;
+echo "/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/"
+sleep 3
+echo "///////////////////////////////////////"
+echo "//                 HASIL             //"
+echo "///////////////////////////////////////"
+echo " http://$sub-1.$web"
+echo " http://$sub-2.$web"
+echo " http://$sub-3.$web"
+echo " http://$sub-4.$web"
+echo " http://$sub-5.$web"
+echo " http://$sub-6.$web"
+echo " http://$sub-7.$web"
+echo " http://$sub-8.$web"
+echo " http://$sub-9.$web"
+echo " http://$sub-10.$web"
+sleep 3
+echo "////////////////SUKSES////////////////"
+echo "// JIKA TIDAK PUAS, GUNAKAN KEMBALI //"
